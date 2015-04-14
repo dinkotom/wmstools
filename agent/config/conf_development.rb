04ff2c94e967ab0b23832cb0715c7c6f2ffb88ae
dinkotom <tomas.dinkov@gmail.com>
2015-04-14 08:25:39 +0200

    Added staging agent config

--- agent/config/conf_development.rb
+++ agent/config/conf_development.rb
@@ -1,4 +1,4 @@
-DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001684/wmstools_staging'
+DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@localhost/wmstools_staging'
 THIS_AGENT_ID = 'localhost'
 SOAPUI_HOME = '/home/dinkotom/Dropbox/projects/wms-tools/trunk/agent/soapui-fake/bin' # without trailing slash
 SVN_HOME = '/home/dinkotom/Dropbox/projects/wms-tools/trunk/agent/svn' # without trailing slash
---
+++ agent/config/conf_staging.rb
@@ -0,0 +1,5 @@
+DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001684/wmstools_staging'
+THIS_AGENT_ID = 'uw001684'
+SOAPUI_HOME = '/home/testAgent/work/soapui' # without trailing slash
+SVN_HOME = '/home/testAgent/work/svn' # without trailing slash
+QUOTA = 5
