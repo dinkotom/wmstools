45eb2f878b26536d7d0d3c7e95aa0d34b5c3ce7d
Kreny <vladan.krenek@seznam.cz>
2015-05-28 17:31:10 +0200

    change configm this will break staging

--- agent/config/conf_production.rb
+++ agent/config/conf_production.rb
@@ -1,6 +1,6 @@
-DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001684/wmstools'
+DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001781/wmstools'
 THIS_AGENT_ID = 'uw001685'
-SOAPUI_HOME = '/home/testAgent/work/soapui' # without trailing slash
-SVN_HOME = '/home/testAgent/work/svn' # without trailing slash
+SOAPUI_HOME = '/root/work/wmsTools/soapui' # without trailing slash
+SVN_HOME = '/root/work/wmsTools/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
 QUOTA = 5
