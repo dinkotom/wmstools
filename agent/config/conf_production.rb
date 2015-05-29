96e43f1ed14385fb132f35a6b4a6694eba1abaa4
Kreny <vladan.krenek@seznam.cz>
2015-05-29 07:35:55 +0200

    increase quota

--- agent/config/conf_production.rb
+++ agent/config/conf_production.rb
@@ -3,4 +3,4 @@ THIS_AGENT_ID = 'uw001685'
 SOAPUI_HOME = '/root/work/wmsTools/soapui' # without trailing slash
 SVN_HOME = '/root/work/wmsTools/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
-QUOTA = 5
+QUOTA = 10
