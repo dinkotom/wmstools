64897f4acada604f1f25acdc6fdb1dc068daaead
Kreny <vladan.krenek@seznam.cz>
2015-05-30 19:05:30 +0200

    try different ports on second agent

--- rakefile.rb
+++ rakefile.rb
@@ -46,7 +46,7 @@ require_relative './rake/stage.rb'
 @production_agent_1.control_port = 8082
 @production_agent_1.change_log_file = '/root/work/wmsTools/agent/agent/deployment'
 @production_agent_1.rack_file = 'agent.ru'
-@production_agent_1.quota = 11
+@production_agent_1.quota = 10
 
 @production_agent_2 = Stage.new
 @production_agent_2.name = 'production_agent_2'
@@ -54,11 +54,11 @@ require_relative './rake/stage.rb'
 @production_agent_2.username = 'root'
 @production_agent_2.password = 'bender'
 @production_agent_2.path = '/root/work/wmsTools/agent'
-@production_agent_2.port = 8081
-@production_agent_2.control_port = 8082
+@production_agent_2.port = 8084
+@production_agent_2.control_port = 8085
 @production_agent_2.change_log_file = '/root/work/wmsTools/agent/agent/deployment'
 @production_agent_2.rack_file = 'agent.ru'
-@production_agent_2.quota = 11
+@production_agent_2.quota = 10
 
 task(:default => [:test, :deploy_staging_server, :deploy_staging_agent]) {}
 
