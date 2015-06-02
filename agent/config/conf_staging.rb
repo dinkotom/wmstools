82cc98799f5d13ea9c78afc729963d575cc78dba
Kreny <vladan.krenek@seznam.cz>
2015-06-02 07:14:38 +0200

    change DB string

--- agent/config/conf_staging.rb
+++ agent/config/conf_staging.rb
@@ -1,4 +1,4 @@
-DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001684/wmstools_staging'
+DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001685/wmstools_staging'
 THIS_AGENT_ID = 'uw001684_staging'
 SOAPUI_HOME = '/home/testAgent/work/soapui' # without trailing slash
 SVN_HOME = '/home/testAgent/work/svn' # without trailing slash
