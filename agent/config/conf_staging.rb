081eb4b3ac25344e14d725f86598404af50b17e9
Kreny <vladan.krenek@seznam.cz>
2015-06-05 14:32:14 +0200

    update JAR_HOME

--- agent/config/conf_staging.rb
+++ agent/config/conf_staging.rb
@@ -3,5 +3,5 @@ THIS_AGENT_ID = 'uw001685_staging'
 SOAPUI_HOME = '/root/work/wmsTools/soapui' # without trailing slash
 SVN_HOME = '/root/work/wmsTools/svn' # without trailing slash
 JAVA_HOME = "#{SOAPUI_HOME}/jre/bin/java" # without trailing slash
-JAR_HOME = '/TestAutomation'
+JAR_HOME = '/root/work/wmsTools/svn/trunk/TestAutomation'
 QUOTA = 5
