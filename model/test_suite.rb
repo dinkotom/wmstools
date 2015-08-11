0139f8676555fad760cd006f69f8e4fe8838f481
Kreny <vladan.krenek@seznam.cz>
2015-08-11 15:42:47 +0200

    single thread task

--- model/test_suite.rb
+++ model/test_suite.rb
@@ -6,6 +6,7 @@ class TestSuite
   property :default_number_of_tests, Integer
   property :priority, Integer
   property :performance, Boolean
+  property :single_thread, Boolean #krenevla, add possibility to run regression tests in single thread
   property :load, Boolean
   property :buffer, Boolean
   property :project_file, String
