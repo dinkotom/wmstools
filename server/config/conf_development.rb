2adff45fc5e83c0883bc4a2bd6cb80add89a5484
Kreny <krenevla@gmail.com>
2016-07-25 09:21:30 +0200

    move scheduler to afternoon

--- server/config/conf_development.rb
+++ server/config/conf_development.rb
@@ -12,7 +12,7 @@ PORT = 8088
 PERFORMANCE_TEST_RESULTS_PER_PAGE = 30
 
 FORTUM_SMOKE_TESTS_JOB = {
-  :cron => '*/10 6-18 * * *',
+  :cron => '*/10 16-23 * * *',
   :suites_environments =>
   [
   {:suite => '[TIT] REGRESSION TESTS 1', :environment => 'FAT'},
