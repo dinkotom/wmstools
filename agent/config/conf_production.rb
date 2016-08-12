f008838a44980ae4b650913b7728ab4fff1ab513
Kreny <krenevla@gmail.com>
2016-08-12 15:04:07 +0200

    x

--- agent/config/conf_production.rb
+++ agent/config/conf_production.rb
@@ -1,4 +1,4 @@
-DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001781/wmstools'
+DATA_SOURCE = 'mysql://wmsuser:SalvatorDali01@uw001837/wmstools'
 THIS_AGENT_ID = 'uw001685'
 SOAPUI_HOME = '/root/work/wmsTools/soapui' # without trailing slash
 SVN_HOME = '/root/work/wmsTools/svn' # without trailing slash
